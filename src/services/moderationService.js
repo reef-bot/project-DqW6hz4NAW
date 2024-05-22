ec003b11224c16d52c14a546adea7aab846b4eed
Bot <126459477+reef-bot@users.noreply.github.com>
2024-05-22 13:12:36 +0530

    Add src/services/moderationService.js

---
+++ src/services/moderationService.js
@@ -0,0 +1,9 @@
+const applyModeration = (message) => {
+  // Logic to apply moderation actions
+};
+
+const logUserAction = (user, action) => {
+  // Logic to log user actions
+};
+
+module.exports = { applyModeration, logUserAction };
